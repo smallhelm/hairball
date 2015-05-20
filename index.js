082c5f9519cb882be92a704ffa6f952ca73561df
Matthew Wright <matthew@smallhelm.com>
2015-05-20 06:44:15 -0600

    done with the essentials

--- index.js
+++ index.js
@@ -1,3 +1,54 @@
-module.exports = {
-  //TODO
+var mori = require('mori');
+var isArray = require('x-is-array');
+
+var normalizePath = function(path){
+  return isArray(path) ? path : [path];
+};
+
+module.exports = function(onChange){
+  var i = mori.hashMap();
+
+  return {
+    get: function(path){
+      path = normalizePath(path);
+      if(path.length === 0){
+        return i;
+      }
+      return mori.getIn(i, path);
+    },
+    put: function(path, value){
+      path = normalizePath(path);
+      value = mori.toClj(value);
+
+      var i2 = path.length === 0 ? value : mori.assocIn(i, path, value);
+      if(onChange){
+        onChange(i2, i, path, value);
+      }
+      i = i2;
+      return i2;
+    },
+    del: function(path){
+      path = normalizePath(path);
+
+      //because dissocIn currently isn't in mori
+      var i2;
+      if(path.length === 0){
+        i2 = mori.hashMap();
+      }else{
+        if(mori.getIn(i, path) == null){//good as gone
+          return i;
+        }
+        var last_key = path.pop();
+        i2 = mori.updateIn(i, path, function(v){
+          return mori.dissoc(v, last_key);
+        });
+        path.push(last_key);//undo the mutation to path
+      }
+      if(onChange){
+        onChange(i2, i, path);
+      }
+      i = i2;
+      return i2;
+    }
+  };
 };
--- tests.js
+++ tests.js
@@ -1,7 +1,55 @@
 var test = require('tape');
-var hairball = require('./index');
+var mori = require('mori');
+var Hairball = require('./index');
 
-test("todo", function(t){
-  t.ok(true);//TODO
+test("get/put/del", function(t){
+  var hb = Hairball();
+
+  var o = {hello: 'world', some: {nested: [{thing: true}, "cool"]}};
+  hb.put([], o);
+
+  t.deepEquals(mori.toJs(hb.get([])), o);
+  t.deepEquals(mori.toJs(hb.get(['some'])), o.some);
+  t.deepEquals(mori.toJs(hb.get(['some', 'nested'])), o.some.nested);
+  t.deepEquals(mori.toJs(hb.get(['some', 'nested', 0])), o.some.nested[0]);
+  t.deepEquals(mori.toJs(hb.get(['some', 'bad', 'path'])), null);
+
+  hb.del(['some', 'bad', 'path', 'shouldn\'t', 'make', 'keys']);
+  t.deepEquals(mori.toJs(hb.get(['some'])), o.some, "del shouldn't make keys if they don't exist");
+
+  hb.put(['some', 'nested', 0], 'not');
+  t.deepEquals(mori.toJs(hb.get(['some', 'nested', 0])), 'not');
+
+  hb.del(['some', 'nested']);
+  t.deepEquals(mori.toJs(hb.get(['some'])), {});
+
+  hb.del([]);
+  t.deepEquals(mori.toJs(hb.get([])), {});
+
+  t.end();
+});
+
+test("onChange", function(t){
+  var history = mori.vector();
+
+  var hb = Hairball(function(new_state, old_state, path, value){
+    history = mori.conj(history, mori.vector(new_state, old_state, mori.toClj(path), value));
+  });
+
+  hb.put([], {hello: 'world'});
+  hb.put(['the', 'house', 'is'], {named: 'cool', siding: 'brick'});
+  hb.del(['the', 'house', 'is', 'named'])
+
+  t.deepEquals(mori.toJs(hb.get([])), {hello: 'world', the: {house: {is: {siding: 'brick'}}}});
+
+  var state1 = {hello: 'world'};
+  var state2 = {hello: 'world', the: {house: {is: {named: 'cool', siding: 'brick'}}}};
+  var state3 = {hello: 'world', the: {house: {is: {siding: 'brick'}}}};
+  var expected = mori.toClj([
+    [state1,     {}, [],                              state1],
+    [state2, state1, ['the', 'house', 'is'],          {named: 'cool', siding: 'brick'}],
+    [state3, state2, ['the', 'house', 'is', 'named'], null]
+  ]);
+  t.ok(mori.equals(expected, history));
   t.end();
 });
